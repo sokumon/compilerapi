fccd344070a934467e188d2292cc30b48bc22d59
sokumon <sohamkulkarns9@gmail.com>
2024-06-01 22:55:24 +0530

    stable state

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = Object.freeze({
+    ID_FILE_PATH: process.cwd()
+});
--- util/genBase64.js
+++ util/genBase64.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+var constants = require('../config')
 function generate(){
     let compile_id = new Array(4);
     let final_string="";
@@ -9,7 +10,8 @@ function generate(){
         compile_id[i]=(characters[random_num])
         final_string=final_string + compile_id[i];
     }
-    let pathofids = path.join(process.cwd(),'ids.txt')
+    let pathofids = path.join(constants.ID_FILE_PATH,'ids.txt')
+    console.log(pathofids)
     fs.readFile(pathofids, function (err, data) {
         if (err) throw err;
         if(data.includes(final_string)){
@@ -17,7 +19,7 @@ function generate(){
         }else{
 	    //Adding a line break
 	    let file_content = final_string + "\n";
-            fs.appendFile('ids.txt', file_content, (err) => {
+            fs.appendFile(pathofids, file_content, (err) => {
                 if (err) throw err;
                 console.log('The "data to append" was appended to file!');
                 return final_string
