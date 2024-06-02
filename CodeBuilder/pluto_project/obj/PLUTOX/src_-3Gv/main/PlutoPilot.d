550559dfeae2e6b71a884ebb383dc50392547884
sokumon <sohamkulkarns9@gmail.com>
2024-06-02 16:48:39 +0530

    new git ignore

---
+++ CodeBuilder/pluto_project/obj/PLUTOX/src_-3Gv/main/PlutoPilot.d
@@ -0,0 +1,29 @@
+obj/PLUTOX/src_-3Gv/main/PlutoPilot.o: src_-3Gv/main/PlutoPilot.cpp \
+ src_-3Gv/includes/PlutoPilot.h platform/Comman.h platform/Control.h \
+ platform/Estimate.h platform/Localisation.h platform/Motor.h \
+ platform/Peripheral.h platform/Specifiers.h platform/Sensor.h \
+ platform/User.h platform/Utils.h platform/XRanging.h
+
+src_-3Gv/includes/PlutoPilot.h:
+
+platform/Comman.h:
+
+platform/Control.h:
+
+platform/Estimate.h:
+
+platform/Localisation.h:
+
+platform/Motor.h:
+
+platform/Peripheral.h:
+
+platform/Specifiers.h:
+
+platform/Sensor.h:
+
+platform/User.h:
+
+platform/Utils.h:
+
+platform/XRanging.h:
