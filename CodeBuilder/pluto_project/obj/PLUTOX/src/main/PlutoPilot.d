f67f6cb23368ef9e6e00b3d53975f03e9f67eb2f
sokumon <sohamkulkarns9@gmail.com>
2024-06-02 16:39:18 +0530

    code compiles now makefile works

--- CodeBuilder/pluto_project/obj/PLUTOX/src/main/PlutoPilot.d
+++ CodeBuilder/pluto_project/obj/PLUTOX/src/main/PlutoPilot.d
@@ -3,15 +3,27 @@ obj/PLUTOX/src/main/PlutoPilot.o: src/main/PlutoPilot.cpp \
  platform/Estimate.h platform/Localisation.h platform/Motor.h \
  platform/Peripheral.h platform/Specifiers.h platform/Sensor.h \
  platform/User.h platform/Utils.h platform/XRanging.h
+
 src/includes/PlutoPilot.h:
+
 platform/Comman.h:
+
 platform/Control.h:
+
 platform/Estimate.h:
+
 platform/Localisation.h:
+
 platform/Motor.h:
+
 platform/Peripheral.h:
+
 platform/Specifiers.h:
+
 platform/Sensor.h:
+
 platform/User.h:
+
 platform/Utils.h:
+
 platform/XRanging.h:
---
+++ CodeBuilder/pluto_project/obj/PLUTOX/src_j6xn/main/PlutoPilot.d
@@ -0,0 +1,29 @@
+obj/PLUTOX/src_j6xn/main/PlutoPilot.o: src_j6xn/main/PlutoPilot.cpp \
+ src_j6xn/includes/PlutoPilot.h platform/Comman.h platform/Control.h \
+ platform/Estimate.h platform/Localisation.h platform/Motor.h \
+ platform/Peripheral.h platform/Specifiers.h platform/Sensor.h \
+ platform/User.h platform/Utils.h platform/XRanging.h
+
+src_j6xn/includes/PlutoPilot.h:
+
+platform/Comman.h:
+
+platform/Control.h:
+
+platform/Estimate.h:
+
+platform/Localisation.h:
+
+platform/Motor.h:
+
+platform/Peripheral.h:
+
+platform/Specifiers.h:
+
+platform/Sensor.h:
+
+platform/User.h:
+
+platform/Utils.h:
+
+platform/XRanging.h:
